85006fba6d413a22ad234b9a52166d0025bb0e2b
jefferson silva <emersons.a296@gmail.com>
2021-07-03 21:31:59 -0300

    Adicionando um Model e um Controller

--- App/Controller/Carros.php
+++ App/Controller/Carros.php
@@ -1,3 +1,18 @@
 <?php
+    session_start();
 
+    use App\Core\Controller;
+    
+    class Carros extends Controller{
+    
+        public function index(){
+    
+            $carrosModel = $this->model("Carros");
+    
+            $carros = $carrosModel->listarTodas();
+    
+            //para mostrar todos os carros possiveis
+           echo json_encode($carros, JSON_UNESCAPED_UNICODE);
+        }
+    }
 ?>
